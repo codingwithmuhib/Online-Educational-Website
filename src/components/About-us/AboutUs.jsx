411c6f90ed15ceeff3bf9f2e62e56c31ed2ad9bf
AMIT ASHOK GUPTA <gamit9022@gmail.com>
2024-06-01 21:04:31 +0530

    new

--- src/components/About-us/AboutUs.jsx
+++ src/components/About-us/AboutUs.jsx
@@ -1,23 +1,24 @@
-import React from "react";
-import "./about.css";
-import { Container, Row, Col } from "reactstrap";
-import aboutImg from "../../assests/images/about-us.png";
-import CountUp from "react-countup";
-import "./about.css";
+import React from 'react';
+import './about.css';
+import { Container, Row, Col } from 'reactstrap';
+import aboutImg from '../../assests/images/about-us.png';
+import CountUp from 'react-countup';
+import './about.css';
 
 const AboutUs = () => {
   return (
     <section>
       <Container>
         <Row>
-          <Col lg="6" md="6">
-            <div className="about__img">
-              <img src={aboutImg} alt="" className="w-100" />
+          <Col lg='6' md='6'>
+            <div className='about__img'>
+              <img src={aboutImg} alt='' className='w-100' />
             </div>
           </Col>
+          {/* Hello  */}
 
-          <Col lg="6" md="6">
-            <div className="about__content">
+          <Col lg='6' md='6'>
+            <div className='about__content'>
               <h2>About Us</h2>
               <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -25,40 +26,40 @@ const AboutUs = () => {
                 explicabo nobis ex quo molestiae!
               </p>
 
-              <div className="about__counter">
-                <div className=" d-flex gap-5 align-items-center">
-                  <div className="single__counter">
-                    <span className="counter">
-                      <CountUp start={0} end={25} duration={2} suffix="K" />
+              <div className='about__counter'>
+                <div className=' d-flex gap-5 align-items-center'>
+                  <div className='single__counter'>
+                    <span className='counter'>
+                      <CountUp start={0} end={25} duration={2} suffix='K' />
                     </span>
 
-                    <p className="counter__title">Completed Projects</p>
+                    <p className='counter__title'>Completed Projects</p>
                   </div>
 
-                  <div className="single__counter">
-                    <span className="counter">
-                      <CountUp start={0} end={12} duration={2} suffix="M" />
+                  <div className='single__counter'>
+                    <span className='counter'>
+                      <CountUp start={0} end={12} duration={2} suffix='M' />
                     </span>
 
-                    <p className="counter__title">Patient Around World</p>
+                    <p className='counter__title'>Patient Around World</p>
                   </div>
                 </div>
 
-                <div className=" d-flex gap-5 align-items-center">
-                  <div className="single__counter">
-                    <span className="counter">
-                      <CountUp start={0} end={95} duration={2} suffix="M" />
+                <div className=' d-flex gap-5 align-items-center'>
+                  <div className='single__counter'>
+                    <span className='counter'>
+                      <CountUp start={0} end={95} duration={2} suffix='M' />
                     </span>
 
-                    <p className="counter__title">Ideas Raised Funds</p>
+                    <p className='counter__title'>Ideas Raised Funds</p>
                   </div>
 
-                  <div className="single__counter">
-                    <span className="counter">
-                      <CountUp start={0} end={5} duration={2} suffix="K" />
+                  <div className='single__counter'>
+                    <span className='counter'>
+                      <CountUp start={0} end={5} duration={2} suffix='K' />
                     </span>
 
-                    <p className="counter__title">Categories Served</p>
+                    <p className='counter__title'>Categories Served</p>
                   </div>
                 </div>
               </div>
